7279f1a42dc843e92bbde23fdc45b42743c7020f
Bloodssa <154530573+Bloodssa@users.noreply.github.com>
2025-05-04 21:22:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function getfromTxt1(){
+        
+    let a = 54.56;
+    let b = a * document.getElementById('myTxt1').value;;
+
+    document.getElementById('get1').innerHTML = b;
+}
+function getfromTxt2(){
+        
+    let c = 0.018;
+    let d = c * document.getElementById('myTxt2').value;;
+
+    document.getElementById('get2').innerHTML = d;
+}
+function getfromTxt3(){
+        
+    let e = 1000;
+    let f = e * document.getElementById('myTxt3').value;;
+
+    document.getElementById('get3').innerHTML = f;
+}
+function getfromTxt4(){
+        
+    let g = 12;
+    let h = g * document.getElementById('myTxt4').value;;
+
+    document.getElementById('get4').innerHTML = h;
+}
+function getfromTxt5(){
+        
+    let i = 2.54;
+    let j = i * document.getElementById('myTxt5').value;;
+
+    document.getElementById('get5').innerHTML = j;
+}
